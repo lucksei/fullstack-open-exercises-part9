76dafd4bf1ae14c576e4b863a87f5f5da6386c2d
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-08-20 08:53:38 -0300

    starting exercise 9.16

--- courses-typescript/src/App.tsx
+++ courses-typescript/src/App.tsx
@@ -1,30 +1,15 @@
-import type { CoursePart } from './types';
-
 import Header from './components/Header';
 import Content from './components/Content';
 import Total from './components/Total';
+import courseParts from './data';
 
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts: CoursePart[] = [
-    {
-      name: 'Fundamentals',
-      exerciseCount: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exerciseCount: 7,
-    },
-    {
-      name: 'Deeper type usage',
-      exerciseCount: 14,
-    },
-  ];
-
   const totalExercises = courseParts.reduce(
     (sum, part) => sum + part.exerciseCount,
     0
   );
+
   return (
     <div>
       <Header courseName={courseName} />
