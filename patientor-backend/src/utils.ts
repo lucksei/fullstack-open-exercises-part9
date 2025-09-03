dd60ca46f4db1b258d7a14942b307ff26a419668
Luca Seimandi <lseimandi@colsecor.com.ar>
2025-09-03 20:26:47 -0300

    exercise 9.23 done -- patients with full entries validated and returned on the individual patient's route

--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,4 +1,4 @@
-import { Gender } from './types';
+import { Gender, HealthCheckRating } from './types';
 import { z } from 'zod';
 
 const newPatientSchema = z.object({
@@ -9,4 +9,49 @@ const newPatientSchema = z.object({
   occupation: z.string(),
 });
 
-export { newPatientSchema };
+const newBaseEntrySchema = z.object({
+  date: z.string(),
+  specialist: z.string(),
+  description: z.string(),
+  diagnosisCodes: z.array(z.string()).optional(),
+});
+
+const newHealthCheckEntrySchema = newBaseEntrySchema.extend({
+  type: z.literal('HealthCheck'),
+  healthCheckRating: z.nativeEnum(HealthCheckRating),
+});
+
+const newOccupationalHealthcareEntrySchema = newBaseEntrySchema.extend({
+  type: z.literal('OccupationalHealthcare'),
+  employerName: z.string(),
+  sickLeave: z
+    .object({
+      startDate: z.string(),
+      endDate: z.string(),
+    })
+    .optional(),
+});
+
+const newHospitalEntrySchema = newBaseEntrySchema.extend({
+  type: z.literal('Hospital'),
+  discharge: z
+    .object({
+      date: z.string(),
+      criteria: z.string(),
+    })
+    .optional(),
+});
+
+const newEntrySchema = z.union([
+  newOccupationalHealthcareEntrySchema,
+  newHospitalEntrySchema,
+  newHealthCheckEntrySchema,
+]);
+
+export {
+  newPatientSchema,
+  newEntrySchema,
+  // newHealthCheckEntrySchema,
+  // newOccupationalHealthcareEntrySchema,
+  // newHospitalEntrySchema,
+};
